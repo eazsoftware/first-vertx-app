102e8cfe8095a8e23b23c0ab631a11854dc72c42
javier-vallejos <Javier.Vallejos@uk.tesco.com>
2017-08-20 17:35:58 +0100

    First commit

---
+++ src/test/java/MyFirstVerticleTest.java
@@ -0,0 +1,60 @@
+
+import com.eaz.firstvertxapp.MyFirstVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.io.IOException;
+import java.net.ServerSocket;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class MyFirstVerticleTest {
+
+    private Vertx vertx;
+
+    private Integer port = 8081;
+
+    @Before
+    public void setUp(TestContext context) throws IOException {
+        vertx = Vertx.vertx();
+
+    // Let's configure the verticle to listen on the 'test' port (randomly picked).
+        // We create deployment options and set the _configuration_ json object:
+        ServerSocket socket = new ServerSocket(0);
+        port = socket.getLocalPort();
+        socket.close();
+        
+        System.out.println("Port: " + port);
+
+        DeploymentOptions options
+                = new DeploymentOptions().setConfig(new JsonObject().put("http.port", port));
+
+        vertx.deployVerticle(MyFirstVerticle.class.getName(),
+                options,
+                context.asyncAssertSuccess());
+    }
+
+    @After
+    public void tearDown(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
+
+    @Test
+    public void testMyApplication(TestContext context) {
+        final Async async = context.async();
+
+        vertx.createHttpClient().getNow(port, "localhost", "/",
+                response -> {
+                    response.handler(body -> {
+                        context.assertTrue(body.toString().contains("Hello"));
+                        async.complete();
+                    });
+                });
+    }
+}
