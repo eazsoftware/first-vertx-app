42c9c22421b07f9e1ae3e2059b72fb591a346e7f
javier-vallejos <Javier.Vallejos@uk.tesco.com>
2017-08-21 21:57:38 +0100

    Playing around with Jenkins: changing the assertion will fail the test

--- src/test/java/MyFirstVerticleTest.java
+++ src/test/java/MyFirstVerticleTest.java
@@ -52,7 +52,7 @@ public class MyFirstVerticleTest {
         vertx.createHttpClient().getNow(port, "localhost", "/",
                 response -> {
                     response.handler(body -> {
-                        context.assertTrue(body.toString().contains("Hello"));
+                        context.assertTrue(body.toString().contains("Hello fails"));
                         async.complete();
                     });
                 });
